d2c6eaeb94fb92ca21fa2d4751b0ee63ed8f4a0b
pranalyadav <yadav.pranal94@gmail.com>
2025-02-07 12:34:42 +0530

    tailwind tutorial

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React from 'react';
+import { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function SearchOrder() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -18,6 +18,7 @@ export default function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
